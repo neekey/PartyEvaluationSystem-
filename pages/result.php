7c73cdec897745dda4cfb233d6147c3075172b4b
neekey <ni184775761@gmail.com>
2012-08-27 22:04:16 +0800

    添加操作结果页面

---
+++ pages/result.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>基层群众欢迎页面</title>
+    <?php include "../mods/assets.common.php"; ?>
+    <link rel="stylesheet" type="text/css" href="../assets/stylesheet/result.css" media="all">
+</head>
+<body>
+<?php include "../mods/header.php"; ?>
+<div id="content">
+    <div class="content-wrap">
+        <div class="side">
+            <ul class="menu">
+                <li class="item"><a href="#" class="menu-button">支部评分</a></li>
+                <li class="item"><a href="#" class="menu-button">修改密码</a></li>
+                <li class="item"><a href="#" class="menu-button">注销登陆</a></li>
+            </ul>
+        </div>
+        <div class="main decorade-border left-border">
+            <div class="block-decoration">
+                <div class="block-decoration-wrap">
+                    <p class="main-info">操作成功</p>
+                    <p class="action"><a href="#">返回欢迎界面</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="footer">
+    <div class="bg">
+        <!--站点通用信息，Copyright等-->
+        <div class="site-info">
+            <?php include "../mods/copyright.php"; ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
