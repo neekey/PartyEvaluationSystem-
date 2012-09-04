32f35dd56dbc0dedd66f6a230cc70de31c8f9bfa
neekey <ni184775761@gmail.com>
2012-09-04 22:02:11 +0800

    添加 学校组织选择学院 页面

---
+++ pages/branch_choose.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>学校组织选择学院</title>
+    <?php include "../mods/assets.common.php"; ?>
+    <link rel="stylesheet" type="text/css" href="../assets/stylesheet/branch_choose.css" media="all">
+</head>
+<body>
+<?php include "../mods/header.php"; ?>
+<div id="content">
+    <div class="content-wrap">
+        <div class="side">
+            <ul class="menu">
+                <li class="item"><a href="#" class="menu-button">支部评分</a></li>
+                <li class="item"><a href="#" class="menu-button">修改密码</a></li>
+                <li class="item"><a href="#" class="menu-button">注销登陆</a></li>
+            </ul>
+        </div>
+        <div class="main decorade-border left-border">
+            <table class="red-table branch-table">
+                <colgroup>
+                    <col>
+                    <col>
+                </colgroup>
+                <tbody>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                    <td><a href="#">计算机科学与技术、软件学院</a></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div id="footer">
+    <div class="bg">
+        <!--站点通用信息，Copyright等-->
+        <div class="site-info">
+            <?php include "../mods/copyright.php"; ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
